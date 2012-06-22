240bbe25e1705d98ecf74f0d1e2700a8131fb59c
Jan Jedrychowski <gogiel@gmail.com>
2012-06-22 22:44:57 +0200

    Session controller JSON response.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        format.html {
+          redirect_to root_url, :notice => "Logged in!"
+        }
+        format.json {
+          render :json => "OK".to_json
+        }
+      else
+        flash.now.alert = "Invalid email or password"
+        format.html {
+          render "new"
+        }
+        format.json {
+          render :json => "FAIL".to_json
+        }
+      end
     end
   end
 
